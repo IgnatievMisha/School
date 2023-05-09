632e09dbf8058a2fc7f0d3ec6a3edd10e98df514
IgnatievMisha <mishaross1604@gmail.com>
2023-05-09 18:56:51 +0300

    chess

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ class School:
         self.students=students #spisok
     def admit_students(self, student):
         self.students.append(student)
-        print(f'{}') #dopisati
+        print(f'{student.name} was admit to school {self.name}') #dopisati
 
 class Student:
+    def __init__(self, name, grade):
+        self.name=name
+        self.grade=grade
+    def promote(self):
+        self.grade+=1
+        print(f'{self.name} was promoted to {self.grade}')
+    def demote(self):
+        self.grade-=1
+        print(f'{self.name} was demote to {self.grade}')
